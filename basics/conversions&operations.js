1fc64007683d383f51a4581006f7a1c0e397c23f
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-06-09 08:12:56 +0000

    conversions&operations

---
+++ basics/conversions&operations.js
@@ -0,0 +1,25 @@
+// "use strict" // treat all js codes as ne wer version
+
+// alert(3+3) // thsi will not eork coz we are using node js not browser..
+
+// some primitive datatypes: int, string, boolean, null-> standalone value, symbol-> unique, undefined
+
+
+// type conversion:
+let score="fdghj";
+console.log(typeof score);
+
+let a=Number(score)
+console.log(typeof a); 
+console.log(a);// when score is string or undefined the value of a is NaN.
+// when score is null then value of a is 0 
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// when isLoggedIn=1 then booleanIsLoggedIn is true,
+    // 0 then false 
+    // "" then false
+    // "sdfgh" the true
