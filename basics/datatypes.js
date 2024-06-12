45b4519c74fae08c25a3ca65f39a15bf902bb6ab
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-06-12 06:59:54 +0000

    strings

---
+++ basics/datatypes.js
@@ -0,0 +1,14 @@
+// strings
+const name="Ary"
+
+const newname=new String("Aryyq-werty") // another way to declare a string. (this will return as a object)
+console.log(newname);
+
+
+// there are various different methods/fucntions in js. we can get them on mdn docs or in browser console.
+console.log(newname.charAt(2))
+console.log(newname.indexOf('y'))
+console.log(newname.slice(-10,7))
+
+console.log(newname.replace('A', 'Z'))
+console.log(newname.split('-')) // this will split the string into array.
