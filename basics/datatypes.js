b132655c0862415fbac7013b3bbb09f184c54f55
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-06-13 10:36:50 +0000

    dates

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -2,13 +2,28 @@
 const name="Ary"
 
 const newname=new String("Aryyq-werty") // another way to declare a string. (this will return as a object)
-console.log(newname);
+// console.log(newname);
 
 
-// there are various different methods/fucntions in js. we can get them on mdn docs or in browser console.
-console.log(newname.charAt(2))
-console.log(newname.indexOf('y'))
-console.log(newname.slice(-10,7))
+// // there are various different methods/fucntions in js. we can get them on mdn docs or in browser console.
+// console.log(newname.charAt(2))
+// console.log(newname.indexOf('y'))
+// console.log(newname.slice(-10,7))
 
-console.log(newname.replace('A', 'Z'))
-console.log(newname.split('-')) // this will split the string into array.
+// console.log(newname.replace('A', 'Z'))
+// console.log(newname.split('-')) // this will split the string into array.
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//DATE and TIME
+
+let d= new Date();
+console.log(typeof d);
+console.log(d);
+console.log(d.toDateString());
+console.log(d.toLocaleString());
+
+let time=Date.now()
+console.log(time);
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Arrays
