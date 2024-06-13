4853b3998bd39c3f2ac6c73e49a747bc83d6033f
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-06-13 11:15:58 +0000

    arrays1

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -16,14 +16,37 @@ const newname=new String("Aryyq-werty") // another way to declare a string. (thi
 
 //DATE and TIME
 
-let d= new Date();
-console.log(typeof d);
-console.log(d);
-console.log(d.toDateString());
-console.log(d.toLocaleString());
+// let d= new Date();
+// console.log(typeof d);
+// console.log(d);
+// console.log(d.toDateString());
+// console.log(d.toLocaleString());
 
-let time=Date.now()
-console.log(time);
+// let time=Date.now()
+// console.log(time);
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Arrays
+let arr1=[4,12,4,8,5]
+let arr2= new Array(7,6,34,53,63,25)
+// methods in array
+arr2.push(100)
+// console.log(arr2)
+
+// let newArr=arr1.join()
+// console.log(arr1)
+// console.log(newArr) // this is in string type
+// console.log(typeof newArr)
+
+// slice and splice(there is a important difference between them. Kindly look the output of both the function before & after using in array )
+console.log("A", arr2)
+const a=arr2.slice(1,3)
+console.log(a)
+
+console.log("B", arr2)
+const b=arr2.splice(1,3)
+console.log(b)
+
+console.log("C", arr2) // in this the all the elements that b array contains will be removed.
+// splice methods overwrites the origuinal array. 
+
