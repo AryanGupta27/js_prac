0a5987d5b401a93c9b178619f2ce7ff3bc93702c
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-07-03 06:12:29 +0000

    more loops

--- basics/array_loops.js
+++ basics/array_loops.js
@@ -12,7 +12,7 @@ map1.set('tn', 'TAmilnaud')
 map1.set('rj', 'rajasthan')
 
 for(const [key,value] of map1){
-    console.log(`Key is ${key} and its value is ${value}`)
+    // console.log(`Key is ${key} and its value is ${value}`)
 }
 
 // for in (it will pick keys/indexes) and mainly used for objects.  
@@ -24,8 +24,8 @@ const myobj={
 }
 
 for (const key in myobj) {
-    console.log(key)
-    console.log(myobj[key])
+    // console.log(key)
+    // console.log(myobj[key])
 
 }
 
@@ -33,9 +33,41 @@ for (const key in myobj) {
 
 const arr2=['js','cpp','py','jv']
 arr2.forEach((element) => { // for each is a higher order function means it will always accept a callback function as a parameter.
-    console.log(element)
+    // console.log(element)
 });
 
 arr2.forEach(function (item){
-    console.log(item)
+    // console.log(item)
+})
+
+function toPrint(elem){
+    // console.log(elem)
+}
+
+arr2.forEach(toPrint) // we can pass a already declared function also.
+
+arr2.forEach((it, index, arr)=>{ // there are three parameters array value, index and array itself.
+    console.log(it, index, arr);
+})
+
+// iterating in array of objects.
+
+const myarr=[
+    {
+        'lang': 'javascript',
+        'extension': 'js'
+    },
+    {
+        'lang1': 'java',
+        'extension': 'java'
+    },
+    {
+        'lang2': 'python',
+        'extension': 'py'
+    }
+]
+
+
+myarr.forEach((obj)=>{
+    console.log(Object.keys(obj));
 })
