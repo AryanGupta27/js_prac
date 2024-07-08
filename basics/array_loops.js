04e2e087af25beb567036e9653b930e6e19f5b73
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-07-08 06:59:42 +0000

    filter,map,reduce

--- basics/array_loops.js
+++ basics/array_loops.js
@@ -6,7 +6,7 @@ const str1="hwllo w"
 for (const i of arr1) {
     console.log(i)
 }
-
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
 const map1= new Map();
 map1.set('tn', 'TAmilnaud')
 map1.set('rj', 'rajasthan')
@@ -14,6 +14,8 @@ map1.set('rj', 'rajasthan')
 for(const [key,value] of map1){
     // console.log(`Key is ${key} and its value is ${value}`)
 }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 // for in (it will pick keys/indexes) and mainly used for objects.  
 
@@ -29,7 +31,10 @@ for (const key in myobj) {
 
 }
 
-// for-each (very useful and important)
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// for-each (very useful and important) 
+// forEach loop does not return anything, so we cannot perform individual operations on each element.
 
 const arr2=['js','cpp','py','jv']
 arr2.forEach((element) => { // for each is a higher order function means it will always accept a callback function as a parameter.
@@ -71,3 +76,63 @@ const myarr=[
 myarr.forEach((obj)=>{
     console.log(Object.keys(obj));
 })
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// filter: filters the data according to condition passed; It also accepts a callback function(High order function).
+//NOTE: It always returns an array that will fulfill the condition.
+
+const newArr=[1,2,3,4,5,6,7,8,9,10]
+// const ot=newArr.filter((item)=>{
+//     return item>5
+// })
+
+const ot=newArr.filter((item)=> item>5) // if we do not use curly braces(scope) then return keyword is not required. 
+console.log(ot)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// map:- It also accepts a callback function(High order function).
+ // it also returns a new array.
+
+ const finalArr=newArr.map((item)=>{
+    return item+10; // this will add 10 on each item of array
+ })
+
+ console.log(finalArr)
+
+ // chaining
+
+ const nums=newArr.map((it)=>it*10)
+                  .map((it)=>it+1)
+                  .filter((it) => it>51)
+console.log(nums)
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//reduce 
+
+const myNums=[1,2,3]
+
+const myTot=myNums.reduce((acc,currval)=>{ // acc is accumulator
+    console.log(`value of acc: ${acc} and value of currval: ${currval}`)
+    return acc+currval
+},0) // after comma this value is initial value that is for accumulator
+console.log(myTot)
+
+const aobj=[
+    {
+        cname:"js",
+        price:700
+    },
+    {
+        cname:"cp",
+        price:500
+    },
+    {
+        cname:"go",
+        price:200
+    }
+]
+
+const re= aobj.reduce((acc,item)=>{return acc+ item.price},0)
+console.log(re);
