2f68ca66d98d26a29f446b86f0568d9ebfc3334e
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-07-14 07:35:45 +0000

    events

---
+++ basics/events/index.js
@@ -0,0 +1,22 @@
+// when we work on events there is a event object which is passes in a callback function.
+// see the below example
+
+// document.querySelector('tagname').addEventListener('click',(e)=>{
+    //     console.log(e) // this e is the event object which has many properties(explore them)
+    // }, false)
+     
+    /* some important properties of events:
+    1. type- type of event
+    2. timestamp
+    3. defaultPrevented
+    4. target
+    5. toElement
+    6. srcElement
+    7. currentTarget
+    8. clientX, clientY
+    9.screenX screenY
+    10. altkey, ctrlkey, shiftkey, keycode
+    
+*/
+
+// NOTE: the addEventListener has 3 parameters last param is by default false.
