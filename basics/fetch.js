ff4ef77b1d8730d53e70fb6000c2a70f9e7047e0
Aryan Gupta <76638534+AryanGupta27@users.noreply.github.com>
2024-07-23 10:23:52 +0000

    fetch

---
+++ basics/fetch.js
@@ -0,0 +1,11 @@
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+ 
+
+// read more about from docs get and post method
